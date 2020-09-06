3b8c73917f1157bb2229f93c546d2aa789f64cd9
muhammadahad96 <muhammadahad9615@gmail.com>
2020-09-07 01:04:16 +0500

    added add-transaction component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Header } from './components/Header';
 import { Balance } from './components/Balance';
 import { IncomeExpenses } from './components/IncomeExpenses';
 import { TransactionList } from './components/TransactionList';
+import { AddTransaction } from './components/AddTransaction';
 
 
 import './App.css';
@@ -15,6 +16,7 @@ function App() {
         <Balance />
         <IncomeExpenses />
         <TransactionList />
+        <AddTransaction />
       </div>
     </div>
   );
--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 export const IncomeExpenses = () => {
   return (
-    <div class="inc-exp-container">
+    <div className="inc-exp-container">
       <div>
         <h4>Income</h4>
-        <p id="money-plus" class="money plus">+PKR 0.00</p>
+        <p className="money plus">+PKR 0.00</p>
       </div>
       <div>
         <h4>Expense</h4>
-        <p id="money-minus" class="money minus">-PKR 0.00</p>
+        <p className="money minus">-PKR 0.00</p>
       </div>
     </div>
   )
