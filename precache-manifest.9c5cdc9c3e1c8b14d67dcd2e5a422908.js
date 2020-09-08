9a2d19054a2fdc93bbea9e296471369d442f1e8b
muhammadahad96 <muhammadahad9615@gmail.com>
2020-09-08 08:45:27 +0500

    Updates

---
+++ precache-manifest.9c5cdc9c3e1c8b14d67dcd2e5a422908.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad3d0c6fb20389c6aee4b747762cc7d1",
+    "url": "/react-expense-tracker/index.html"
+  },
+  {
+    "revision": "2d029f879950be323760",
+    "url": "/react-expense-tracker/static/css/main.71def9ba.chunk.css"
+  },
+  {
+    "revision": "33556b952ec193449c6c",
+    "url": "/react-expense-tracker/static/js/2.80f3d389.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-expense-tracker/static/js/2.80f3d389.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d029f879950be323760",
+    "url": "/react-expense-tracker/static/js/main.7903ab4c.chunk.js"
+  },
+  {
+    "revision": "78c83d7ef0dc2332c961",
+    "url": "/react-expense-tracker/static/js/runtime-main.f8b53f9c.js"
+  }
+]);
